75ab9a5c973056a4a2ce26708cc770dd23101c3b
kiranmukherjee <77300690+kiranmukherjee@users.noreply.github.com>
2021-01-26 19:50:23 -0800

    attempt to link script

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
 
 let myInput = document.getElementById("usernumber"); 
 
-console.log(myInput)
+// console.log(myInput)
 
 let submission = document.getElementById("submit"); 
 
-
 submission.addEventListener("click", function(){
     console.log(myInput.value);
-    
-
 })
 
-// var x = document.createElement("input");
 
-// x.setAttribute("type", "number");
+if (usernumber<11){
+  console.log( "you have reached option 1" );
+}
+else if (usernumber>11 && usernumber<33){
+    console.log("you have reached option 2");
+}   
 
-// if (myNumber<11){
-//   console.log( "you have reached option 1" );
-// }
-// else if (myNumber>11 && myNumber<33){
-//     console.log("you have reached option 2");
-// }   
 
-// if (myNumber == 11){
-//   console.log("you win! you chose the correct number");
-// }
-// //
+if (usernumber == 11){
+  console.log("you win! you chose the correct number");
+}
