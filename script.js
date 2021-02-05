7b228e661607317f3ea5d01d24912d071520490d
kiranmukherjee <77300690+kiranmukherjee@users.noreply.github.com>
2021-02-05 14:07:16 -0800

    working site

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// button 1
 button1 = document.getElementById("btn1")
 
 function randomword(){
-    let words = ['Dog', 'Cat', 'Frog', 'Rat', 'Gerbil'];
+    let words = ['I', 'You', 'He', 'She', 'They'];
     let word = words[Math.floor(Math.random() * 5)];
     return word;
      
@@ -11,10 +12,11 @@ button1.onclick = function() {
     document.getElementById("txtOutput").innerHTML = randomword()
 }
 
+// button 2
 button2 = document.getElementById("btn2")
 
 function randomword2(){
-    let words = ['Dog', 'Cat', 'Frog', 'Rat', 'Gerbil'];
+    let words = ['went', 'came', 'drove', 'biked', 'flew'];
     let word = words[Math.floor(Math.random() * 5)];
     return word;
      
@@ -22,6 +24,34 @@ function randomword2(){
 
 button2.onclick = function() {
     document.getElementById("txtOutput2").innerHTML = randomword2()
+}
+
+// button 3
+button3 = document.getElementById("btn3")
+
+function randomword3(){
+    let words = ['to', 'from', 'along'];
+    let word = words[Math.floor(Math.random() * 3)];
+    return word;
+     
+};
+
+button3.onclick = function() {
+    document.getElementById("txtOutput3").innerHTML = randomword3()
+}
+
+// button 4
+button4 = document.getElementById("btn4")
+
+function randomword4(){
+    let words = ['the ocean.', 'the street.', 'downtown.', 'the river.', 'the mountain.'];
+    let word = words[Math.floor(Math.random() * 5)];
+    return word;
+     
+};
+
+button4.onclick = function() {
+    document.getElementById("txtOutput4").innerHTML = randomword4()
 }
 
 // let txtOutput = document.getElementById("txtOutput");
